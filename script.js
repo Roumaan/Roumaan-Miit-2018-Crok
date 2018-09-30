1944b854a81f1f7a8814a442cf262f628bf0eaac
Roumaan <romanchik_2002@mail.ru>
2018-09-30 12:32:42 +0300

    reg new fix

--- script.js
+++ script.js
@@ -5,31 +5,31 @@ function check() {
     
     let login = form.login.value;
     if (login == "") {
-        form.logError.style.visibility = "visible"
+        form.logError.style.display = "inline";
         form.logError.title = "Это поле обязательно"
         isRight = false;
     } else {
-        form.logError.style.visibility = "hidden";
+        form.logError.style.display = "none";
         form.logError.title="";
     }
     
     let email = form.email.value;
     
     if (email == "") {
-        form.emError.style.visibility = "visible";
+        form.emError.style.display = "inline";
         form.emError.title = "Это поле обязательно";
         isRight = false;
     } else if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(email)) {
-        form.emError.style.visibility = "visible";
+        form.emError.style.display = "inline"
         form.emError.title = "Адрес должен быть вида example@example.com";
     } else {
-        form.emError.style.visibility = "hidden";
+        form.emError.style.display = "none";
         form.emError.title="";
     }
     
     let password1 = form.password1.value;    
     if (password1 == "") {
-        form.pasError.style.visibility = "visible";
+        form.pasError.style.display = "inline"
         form.pasError.title = "Это поле обязательно";
         isRight = false;   
     } else {
@@ -43,26 +43,26 @@ function check() {
         if (!/(?=.*[A-Z])/g.test(password1))
             pasError+="\n-Пароль должен cодержать хотя бы одну латинскую букву в верхнем регистре";
         if (pasError != '') {
-            form.pasError.style.visibility = "visible";
+            form.pasError.style.display = "inline"
             form.pasError.title = pasError;
             isRight = false;
         } else {
-        form.pasError.style.visibility = "hidden";
+        form.pasError.style.display = "none";
         form.pasError.title = pasError;
         }
     }
     
     let password2 = form.password2.value;
     if (password2 == "") {
-        form.pasEqError.style.visibility = "visible";
+        form.pasEqError.style.display = "inline"
         form.pasEqError.title = "Это поле обязательно";
         isRight = false;   
     } else if (password1 != password2) {
-        form.pasEqError.style.visibility = "visible";
+        form.pasEqError.style.display = "inline"
         form.pasEqError.title="-Пароли должны совпадать";
         isRight = false;
     } else {
-        form.pasEqError.style.visibility = "hidden";
+        form.pasEqError.style.display = "none";
         form.pasEqError.title="";
     }
     return isRight;
