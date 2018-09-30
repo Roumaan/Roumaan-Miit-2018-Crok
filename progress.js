9a49504f0af5975531b9e3496e1283b76779ff2c
Roumaan <romanchik_2002@mail.ru>
2018-09-30 12:32:29 +0300

    progressbar

---
+++ progress.js
@@ -0,0 +1,24 @@
+function conf_progr(obj, val) {
+    let back = obj.children[0];
+    let front = back.children[0];
+    let percents = obj.children[1];
+    
+    let x = 0;
+    let id = setInterval(prog_dym, 10)
+    function prog_dym() {
+        if (x >= val || x >= 100) {
+            clearInterval(id)
+        } else {
+            x++;
+            front.style.width = x + '%'
+            percents.innerHTML = x +'%'
+            percents.style.marginLeft = 0.01*x* parseInt(getComputedStyle(back).width) - 7 + 'px';
+        }
+    }
+}
+
+let bars = document.getElementsByClassName('progress');
+for (let i = 0; i < bars.length; i++) {
+     conf_progr(bars[i], Math.random()*100);
+}
+
